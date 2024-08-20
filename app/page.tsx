1f300250dd726307378eb9fc33b058a22774fabc
sonylomo <sonylomo1@gmail.com>
2024-08-20 12:47:51 +0300

    fix: build errors

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,5 @@
 "use client";
 
-import BlogCard from "../components/blogCard";
-import { BlogData } from "../data/blogData";
 import {
   ArrowBackIcon,
   ChevronLeftIcon,
@@ -13,7 +11,6 @@ import {
   Button,
   Center,
   Container,
-  Grid,
   Input,
   InputGroup,
   InputRightElement,
@@ -23,8 +20,10 @@ import {
   VStack,
 } from "@chakra-ui/react";
 import { usePathname, useRouter } from "next/navigation";
-import { useState } from "react";
+import { Suspense, useState } from "react";
 import { useDebouncedCallback } from "use-debounce";
+import BlogCard from "../components/blogCard";
+import { BlogData } from "../data/blogData";
 
 const POSTS_PER_PAGE = 6;
 
@@ -80,104 +79,106 @@ const Home = ({
   }, 300);
 
   return (
-    <Container as={"main"} maxW={"1280px"}>
-      {/* {Only appears when you start searching} */}
-      {query && (
-        <>
-          <Link
-            href="/"
-            display={"flex"}
-            gap={21}
-            alignItems={"center"}
-            _hover={{ color: "gray.secondary", textDecoration: "underline" }}
-            py={10}
+    <Suspense fallback={<div>Loading...</div>}>
+      <Container as={"main"} maxW={"1280px"}>
+        {/* {Only appears when you start searching} */}
+        {query && (
+          <>
+            <Link
+              href="/"
+              display={"flex"}
+              gap={21}
+              alignItems={"center"}
+              _hover={{ color: "gray.secondary", textDecoration: "underline" }}
+              py={10}
+            >
+              <ArrowBackIcon />
+              <Text fontWeight={500}>Back to blog posts</Text>
+            </Link>
+            <VStack mx={"auto"} spacing={8}>
+              <Text fontWeight={700} fontSize={32}>
+                Search Blogs
+              </Text>
+
+              <InputGroup size="lg" mx={"auto"} w={540}>
+                <Input
+                  pr="4.5rem"
+                  type="text"
+                  placeholder="Search Blog"
+                  borderColor={"#000000"}
+                  _hover={{
+                    borderColor: "#000000",
+                  }}
+                  onChange={(e) => {
+                    handleSearch(e.target.value);
+                  }}
+                  defaultValue={query}
+                />
+                <InputRightElement width="4.5rem">
+                  <SearchIcon />
+                </InputRightElement>
+              </InputGroup>
+
+              <Text fontWeight={700}>
+                Showing {selectedPosts.length} Results of{" "}
+                <strong>&quot;{query}&quot;</strong>
+              </Text>
+            </VStack>
+          </>
+        )}
+
+        <Box py={10}>
+          <Text fontWeight={700} fontSize={"4xl"}>
+            The Accessibility Blog
+          </Text>
+          <Text>The voice of the excluded</Text>
+        </Box>
+        <SimpleGrid columns={{ sm: 2, lg: 3 }} gap={{ base: 10, lg: 20 }}>
+          {selectedPosts.map(({ id, title, publicationDate, excerpt }) => (
+            <BlogCard
+              key={id}
+              id={id}
+              title={title}
+              publicationDate={publicationDate}
+              excerpt={excerpt}
+            />
+          ))}
+        </SimpleGrid>
+
+        {/* Pagination */}
+        <Center gap={10} py={[10, 20]} mx={"auto"}>
+          <Button
+            aria-label="Previous page"
+            onClick={handlePreviousPage}
+            disabled={currentPage === 1}
+            bg={"transparent"}
+            border={"1px"}
+            borderColor={"#012B55"}
+            borderRadius={4}
+            px={1}
+            _hover={{ bg: "black", color: "white", borderColor: "white" }}
           >
-            <ArrowBackIcon />
-            <Text fontWeight={500}>Back to blog posts</Text>
-          </Link>
-          <VStack mx={"auto"} spacing={8}>
-            <Text fontWeight={700} fontSize={32}>
-              Search Blogs
-            </Text>
-
-            <InputGroup size="lg" mx={"auto"} w={540}>
-              <Input
-                pr="4.5rem"
-                type="text"
-                placeholder="Search Blog"
-                borderColor={"#000000"}
-                _hover={{
-                  borderColor: "#000000",
-                }}
-                onChange={(e) => {
-                  handleSearch(e.target.value);
-                }}
-                defaultValue={query}
-              />
-              <InputRightElement width="4.5rem">
-                <SearchIcon />
-              </InputRightElement>
-            </InputGroup>
-
-            <Text fontWeight={700}>
-              Showing {selectedPosts.length} Results of{" "}
-              <strong>&quot;{query}&quot;</strong>
-            </Text>
-          </VStack>
-        </>
-      )}
-
-      <Box py={10}>
-        <Text fontWeight={700} fontSize={"4xl"}>
-          The Accessibility Blog
-        </Text>
-        <Text>The voice of the excluded</Text>
-      </Box>
-      <SimpleGrid columns={{ sm: 2, lg: 3 }} gap={{ base: 10, lg: 20 }}>
-        {selectedPosts.map(({ id, title, publicationDate, excerpt }) => (
-          <BlogCard
-            key={id}
-            id={id}
-            title={title}
-            publicationDate={publicationDate}
-            excerpt={excerpt}
-          />
-        ))}
-      </SimpleGrid>
-
-      {/* Pagination */}
-      <Center gap={10} py={[10, 20]} mx={"auto"}>
-        <Button
-          aria-label="Previous page"
-          onClick={handlePreviousPage}
-          disabled={currentPage === 1}
-          bg={"transparent"}
-          border={"1px"}
-          borderColor={"#012B55"}
-          borderRadius={4}
-          px={1}
-          _hover={{ bg: "black", color: "white", borderColor: "white" }}
-        >
-          <ChevronLeftIcon w={13} h={13} />
-        </Button>
-        <Text>
-          Page {currentPage} of {totalPages}
-        </Text>
-        <Button
-          aria-label="Next page"
-          onClick={handleNextPage}
-          disabled={currentPage === totalPages}
-          bg={"transparent"}
-          border={"1px"}
-          borderColor={"#012B55"}
-          borderRadius={4}
-          px={1}
-          _hover={{ bg: "black", color: "white", borderColor: "white" }}
-        >
-          <ChevronRightIcon />
-        </Button>
-      </Center>
-    </Container>
+            <ChevronLeftIcon w={13} h={13} />
+          </Button>
+          <Text>
+            Page {currentPage} of {totalPages}
+          </Text>
+          <Button
+            aria-label="Next page"
+            onClick={handleNextPage}
+            disabled={currentPage === totalPages}
+            bg={"transparent"}
+            border={"1px"}
+            borderColor={"#012B55"}
+            borderRadius={4}
+            px={1}
+            _hover={{ bg: "black", color: "white", borderColor: "white" }}
+          >
+            <ChevronRightIcon />
+          </Button>
+        </Center>
+      </Container>
+    </Suspense>
   );
 };
 
--- components/navbar.tsx
+++ components/navbar.tsx
@@ -14,6 +14,7 @@ import {
 } from "@chakra-ui/react";
 import Image from "next/image";
 import { usePathname, useRouter, useSearchParams } from "next/navigation";
+import { Suspense } from "react";
 import { useDebouncedCallback } from "use-debounce";
 
 const NavBar = () => {
@@ -34,31 +35,99 @@ const NavBar = () => {
   }, 200);
 
   return (
-    <Box bg="gray.primary" px={[4, 70]} py={15} as={"nav"}>
-      <Flex h={16} alignItems={"center"} justifyContent={"space-between"}>
-        <Box>
-          <Image
-            src="/logo.svg"
-            alt="Blog Posts Logo"
-            width="144"
-            height="57"
-            priority
-          />
-        </Box>
+    <Suspense fallback={<div>Loading...</div>}>
+      <Box bg="gray.primary" px={[4, 70]} py={15} as={"nav"}>
+        <Flex h={16} alignItems={"center"} justifyContent={"space-between"}>
+          <Box>
+            <Image
+              src="/logo.svg"
+              alt="Blog Posts Logo"
+              width="144"
+              height="57"
+              priority
+            />
+          </Box>
 
-        <Flex alignItems={"center"}>
-          <HStack
-            as={"nav"}
-            spacing={8}
-            mx={10}
-            display={{ base: "none", md: "flex" }}
-          >
+          <Flex alignItems={"center"}>
+            <HStack
+              as={"nav"}
+              spacing={8}
+              mx={10}
+              display={{ base: "none", md: "flex" }}
+            >
+              <Box
+                as="a"
+                px={4}
+                py={1}
+                fontWeight={500}
+                rounded={"md"}
+                _hover={{
+                  textDecoration: "none",
+                }}
+                href={"/"}
+              >
+                Blogs
+              </Box>
+              <Box>
+                <InputGroup size="md">
+                  <Input
+                    pr="4.5rem"
+                    type="text"
+                    placeholder="Search Blog"
+                    borderColor={"#000000"}
+                    _hover={{
+                      borderColor: "#000000",
+                    }}
+                    onChange={(e) => {
+                      handleSearch(e.target.value);
+                    }}
+                    defaultValue={searchParams.get("query")?.toString()}
+                  />
+                  <InputRightElement width="4.5rem">
+                    <SearchIcon />
+                  </InputRightElement>
+                </InputGroup>
+              </Box>
+            </HStack>
+            <Button
+              variant={"solid"}
+              bg={"gray.secondary"}
+              color={"white"}
+              size={"md"}
+              mr={4}
+              borderRadius={4}
+              _hover={{
+                bg: "transparent",
+                color: "gray.secondary",
+                border: "1px",
+                borderColor: "gray.secondary",
+              }}
+              onClick={() => router.push("/posts/create")}
+            >
+              Create Blog Post
+            </Button>
+          </Flex>
+
+          {/* Hamburger displayed in mobile view */}
+          <IconButton
+            size={"md"}
+            icon={isOpen ? <CloseIcon /> : <HamburgerIcon />}
+            aria-label={"Open Menu"}
+            display={{ md: "none" }}
+            onClick={isOpen ? onClose : onOpen}
+            bg={"gray.secondary"}
+          />
+        </Flex>
+
+        {/* Mobile display */}
+        {isOpen && (
+          <Box pt={4} display={{ md: "none" }}>
             <Box
               as="a"
               px={4}
               py={1}
-              fontWeight={500}
               rounded={"md"}
+              fontWeight={700}
               _hover={{
                 textDecoration: "none",
               }}
@@ -66,76 +135,10 @@ const NavBar = () => {
             >
               Blogs
             </Box>
-            <Box>
-              <InputGroup size="md">
-                <Input
-                  pr="4.5rem"
-                  type="text"
-                  placeholder="Search Blog"
-                  borderColor={"#000000"}
-                  _hover={{
-                    borderColor: "#000000",
-                  }}
-                  onChange={(e) => {
-                    handleSearch(e.target.value);
-                  }}
-                  defaultValue={searchParams.get("query")?.toString()}
-                />
-                <InputRightElement width="4.5rem">
-                  <SearchIcon />
-                </InputRightElement>
-              </InputGroup>
-            </Box>
-          </HStack>
-          <Button
-            variant={"solid"}
-            bg={"gray.secondary"}
-            color={"white"}
-            size={"md"}
-            mr={4}
-            borderRadius={4}
-            _hover={{
-              bg: "transparent",
-              color: "gray.secondary",
-              border: "1px",
-              borderColor: "gray.secondary",
-            }}
-            onClick={() => router.push("/posts/create")}
-          >
-            Create Blog Post
-          </Button>
-        </Flex>
-
-        {/* Hamburger displayed in mobile view */}
-        <IconButton
-          size={"md"}
-          icon={isOpen ? <CloseIcon /> : <HamburgerIcon />}
-          aria-label={"Open Menu"}
-          display={{ md: "none" }}
-          onClick={isOpen ? onClose : onOpen}
-          bg={"gray.secondary"}
-        />
-      </Flex>
-
-      {/* Mobile display */}
-      {isOpen && (
-        <Box pt={4} display={{ md: "none" }}>
-          <Box
-            as="a"
-            px={4}
-            py={1}
-            rounded={"md"}
-            fontWeight={700}
-            _hover={{
-              textDecoration: "none",
-            }}
-            href={"/"}
-          >
-            Blogs
           </Box>
-        </Box>
-      )}
-    </Box>
+        )}
+      </Box>
+    </Suspense>
   );
 };
 
